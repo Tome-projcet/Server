8655b0bf48e40ce10bf77824641b609ec52f78c2
Tortoise <85627881k@gmail.com>
2026-09-16 20:19:24 +0900

    feat: UserGenre 생성

--- src/main/java/com/Tome/tome/config/jwt/TokenProvider.java
+++ src/main/java/com/Tome/tome/config/jwt/TokenProvider.java
@@ -24,18 +24,18 @@ public class TokenProvider {
 
     private final JwtProperties jwtProperties;
 
-    public String generateToken(User user, Duration expriedAt){
+    public String generateToken(User user, Duration expiredAt){
         Date now = new Date();
-        return makeToken(new Date(now.getTime() + expriedAt.toMillis()),user);
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()),user);
     }
 
-    private String makeToken(Date expriy, User user){
+    private String makeToken(Date expiry, User user){
         Date now = new Date();
 
         return Jwts.builder().setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setIssuer(jwtProperties.getIssuer())
                 .setIssuedAt(now)
-                .setExpiration(expriy)
+                .setExpiration(expiry)
                 .setSubject(user.getEmail())
                 .claim("id", user.getId())
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
